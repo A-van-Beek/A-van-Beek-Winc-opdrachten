2fee9fe0048f35a1c95e520bff70d85cc9eae358
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-06-07 12:18:33 +0200

    add 044, JS_array_methods_1

---
+++ 044_JS_array_methods_1/index.js
@@ -0,0 +1,19 @@
+// /*
+// voeg woord toe aan array
+// */
+// const addTheWordCool = (myArray) => {
+//   myArray.push("cool");
+//   console.log("Add cool: ", myArray);
+// };
+
+// addTheWordCool(["nice", "awesome", "tof"]);
+
+/*
+retour aantal elementen van array
+*/
+const amountOfElementsInArray = (myArray) => {
+  //   console.log(myArray);
+  myArray.length;
+};
+
+console.log(amountOfElementsInArray(["appels", "peren", "citroenen"]));

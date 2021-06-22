b9bc91ee7967b259fe691d3a6532cfab4339da01
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-06-22 14:14:36 +0200

    add posters in HTML

--- 048_JS_eindopdracht/script.js
+++ 048_JS_eindopdracht/script.js
@@ -1,14 +1,34 @@
+// /*
+//         functie die een nieuw li-element maakt.
+// */
+// function createMenuItem(name) {
+//   const li = document.createElement("li");
+//   li.textContent = name;
+//   return li;
+// }
+
+// const addMoviesToDom = movies.map((movie) => {
+//   const newMovie = document.getElementById("movies"); //get <ul> element
+//   const allMovies = movie.Title; //get titel van nw movie
+//   newMovie.appendChild(createMenuItem(allMovies)).classList.add("tile");
+// });
+
 /*
-        functie die een nieuw li-element maakt.
+        functie die in nieuw li-element het juiste img zet.
 */
-function createMenuItem(name) {
-  const li = document.createElement("li");
-  li.textContent = name;
-  return li;
-}
+// function myFunction() {
+//   const x = document.createElement("IMG");
+//   x.setAttribute("src", "https://m.media-amazon.com/images/M/MV5BZGIzNWYzN2YtMjcwYS00YjQ3LWI2NjMtOTNiYTUyYjE2MGNkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg");
+//   x.setAttribute("alt", "The Pulpit Rock");
+//   document.body.appendChild(x);
+// }
 
 const addMoviesToDom = movies.map((movie) => {
   const newMovie = document.getElementById("movies"); //get <ul> element
-  const allMovies = movie.Title; //get titel van nw movie
-  newMovie.appendChild(createMenuItem(allMovies)).classList.add("tile");
+  const newLi = document.createElement("li"); // create nw <li>
+  newMovie.appendChild(newLi).classList.add("tile"); //combine <li> in <ul>
+  const newImg = document.createElement("IMG"); // create nw <img>
+  newImg.setAttribute("src", movie.Poster); //add link to <img>
+  newImg.setAttribute("alt", movie.Title); //add alt-text to <img>
+  newLi.appendChild(newImg); //combine nw <img> in nw <li>
 });

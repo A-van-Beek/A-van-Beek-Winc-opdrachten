f2af74f4c582d5516dfe70ea611d14ca7e69af64
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-07-19 12:34:24 +0200

    delete foute map

--- 058_API_key_private/api-client.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = "cde09870aeadbfab23c4cd701f2bf88b";
-// getData
-async function getData(url) {
-  const response = await fetch(url)
-    .then((answer) => answer.json())
-    .catch((err) => console.log(err));
-  console.log(response); //laat resultaat zien in console
-  return response;
-}

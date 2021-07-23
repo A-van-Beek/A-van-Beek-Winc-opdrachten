96edc6f41d5bbfdd85dd30398a7f06d6536413dc
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-07-23 12:03:11 +0200

    rewrite url's

--- 058_API_key_final/api-client.js
+++ 058_API_key_final/api-client.js
@@ -1,5 +1,8 @@
 const API_KEY = "cde09870aeadbfab23c4cd701f2bf88b";
+const API_KEY_2 = "api_key=cde09870aeadbfab23c4cd701f2bf88b";
 const taal = "nl";
+const taal_2 = "&language=nl";
+const movieDB = "https://api.themoviedb.org/3";
 
 // getData
 async function getData(url) {
--- 058_API_key_final/script.js
+++ 058_API_key_final/script.js
@@ -1,82 +1,86 @@
-// const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=${taal}`;
+const genreUrl = movieDB + "/genre/movie/list?" + API_KEY_2 + taal_2;
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   try {
-//     const genresList = document.getElementById("genres");
-//     const genres = await getData(genreUrl);
-//     // console.log(genres.genres[1].name);
-//     genres.genres.forEach((genre) => {
-//       let newLi = document.createElement("li");
-//       newLi.innerHTML = `Genre: ${genre.name}, Id: ${genre.id}`;
-//       genresList.append(newLi);
-//     });
-//   } catch (err) {
-//     console.log("Error! (genresList)");
-//     console.log(err);
-//   }
-// });
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const genresList = document.getElementById("genres");
+    const genres = await getData(genreUrl);
+    // console.log(genres.genres[1].name);
+    genres.genres.forEach((genre) => {
+      let newLi = document.createElement("li");
+      newLi.innerHTML = `Genre: ${genre.name}, Id: ${genre.id}`;
+      genresList.append(newLi);
+    });
+  } catch (err) {
+    console.log("Error! (genresList)");
+    console.log(err);
+  }
+});
 
-// // //
-// // zoeken op zoektermen
-// // niet hoofdletter gevoelig
-// const keywordUrl =
-//   "https://api.themoviedb.org/3/search/movie?api_key=cde09870aeadbfab23c4cd701f2bf88b&query=Kangaroo+kid&language=nl";
+// //
+// zoeken op zoektermen
+// niet hoofdletter gevoelig
+const keywordUrl =
+  movieDB + "/search/movie?" + API_KEY_2 + taal_2 + "&query=Kangaroo+kid";
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   try {
-//     const keywordList = document.getElementById("keyword");
-//     const keywords = await getData(keywordUrl);
-//     keywords.results.forEach((result) => {
-//       let newLi = document.createElement("li");
-//       newLi.innerHTML = `Originele titel: ${result.original_title}, release datum ${result.release_date}`;
-//       keywordList.append(newLi);
-//     });
-//   } catch (err) {
-//     console.log("Error! (keywordList)");
-//     console.log(err);
-//   }
-// });
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const keywordList = document.getElementById("keyword");
+    const keywords = await getData(keywordUrl);
+    keywords.results.forEach((result) => {
+      let newLi = document.createElement("li");
+      newLi.innerHTML = `Originele titel: ${result.original_title}, release datum ${result.release_date}`;
+      keywordList.append(newLi);
+    });
+  } catch (err) {
+    console.log("Error! (keywordList)");
+    console.log(err);
+  }
+});
 
 // // //
 // // zoeken op movie_id
 // //
-// const movieIdUrl =
-//   "https://api.themoviedb.org/3/movie/578?api_key=cde09870aeadbfab23c4cd701f2bf88b&language=nl";
+const movieIdUrl = movieDB + "/movie/578?" + API_KEY_2 + taal_2;
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   try {
-//     const movieIdList = document.getElementById("movie_id");
-//     const movieId = await getData(movieIdUrl);
-//     let newLi = document.createElement("li");
-//     newLi.innerHTML = `Titel: ${movieId.original_title}, id: ${movieId.id}`;
-//     movieIdList.append(newLi);
-//   } catch (err) {
-//     console.log("Error! (movieIdList)");
-//     console.log(err);
-//   }
-// });
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const movieIdList = document.getElementById("movie_id");
+    const movieId = await getData(movieIdUrl);
+    let newLi = document.createElement("li");
+    newLi.innerHTML = `Titel: ${movieId.original_title}, id: ${movieId.id}`;
+    movieIdList.append(newLi);
+  } catch (err) {
+    console.log("Error! (movieIdList)");
+    console.log(err);
+  }
+});
 
 // // //
 // // presenteer favoriet
 // //
-// const favorietUrl =
-//   "https://api.themoviedb.org/3/movie/577?api_key=cde09870aeadbfab23c4cd701f2bf88b&language=nl";
+const favorietUrl = movieDB + "/movie/577?" + API_KEY_2 + taal_2;
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   try {
-//     const favorietId = await getData(favorietUrl);
-//     document.getElementById("favoriet").innerHTML = favorietId.original_title;
-//   } catch (err) {
-//     console.log("Error! (favoriet)");
-//     console.log(err);
-//   }
-// });
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const favorietId = await getData(favorietUrl);
+    document.getElementById("favoriet").innerHTML = favorietId.original_title;
+  } catch (err) {
+    console.log("Error! (favoriet)");
+    console.log(err);
+  }
+});
 
+// // //
+// // presenteer film via find vanuit andere database (in dit geval IMDb)
 // //
-// presenteer film via find vanuit andere database (in dit geval IMDb)
-//
 const findUrl =
-  "https://api.themoviedb.org/3/find/tt1230545?api_key=cde09870aeadbfab23c4cd701f2bf88b&language=nl&external_source=imdb_id";
+  movieDB +
+  "/find/tt1230545?" +
+  API_KEY_2 +
+  taal_2 +
+  "&external_source=imdb_id";
+
+// "https://api.themoviedb.org/3/find/tt1230545?api_key=cde09870aeadbfab23c4cd701f2bf88b&language=nl&external_source=imdb_id";
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
@@ -93,7 +97,15 @@ document.addEventListener("DOMContentLoaded", async () => {
 // presenteer films op populariteit descending
 //
 
-const popularUrl = `https://api.themoviedb.org/3/discover/movie?api_key=cde09870aeadbfab23c4cd701f2bf88b&language=nl&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
+const popularUrl =
+  movieDB +
+  "/discover/movie?" +
+  API_KEY_2 +
+  taal_2 +
+  "&sort_by=popularity.desc" +
+  "&include_adult=false" +
+  "&include_video=false" +
+  "&page=1";
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {

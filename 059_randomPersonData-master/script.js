292e065e506f5e3bcce56ba30f62ff6bd2dc2e74
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-07-27 14:50:07 +0200

    opdracht deel 1

--- 059_randomPersonData-master/script.js
+++ 059_randomPersonData-master/script.js
@@ -1,2 +1,27 @@
+// maak array met landen
+
 // hier het script
-console.log("Hallo");
+var regionList = [];
+var uniqueRegion = [];
+
+landenlijst.addEventListener("click", createRegionList);
+
+function createRegionList() {
+  // create list regions
+  randomPersonData.forEach((person) => {
+    regionList.push(person.region);
+  });
+  // ondubbelen
+  var uniqueRegion = [...new Set(regionList)];
+  // sort
+  uniqueRegion.sort();
+  uniqueRegion.forEach((region) => {
+    const newLi = document.createElement("li");
+    const menuAlleLanden = document.getElementById("landen");
+    menuAlleLanden.appendChild(
+      newLi,
+      menuAlleLanden.getElementsByTagName("li")[0]
+    );
+    newLi.innerHTML = region;
+  });
+}

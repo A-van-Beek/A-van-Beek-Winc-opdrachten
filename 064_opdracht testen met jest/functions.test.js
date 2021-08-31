a5fe0ce225cda89f1a6b4f7bfb415dfec64b2b5d
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-08-31 15:47:08 +0200

    add checks

--- 064_opdracht testen met jest/functions.test.js
+++ 064_opdracht testen met jest/functions.test.js
@@ -1,40 +1,41 @@
-//Bestand functions.js
-module.exports = functions;
-
 //Bestand functions.test.js:
+const { expect } = require("@jest/globals");
+const { checkValue } = require("./functions.js");
 const functions = require("./functions.js");
 
 test("Should be null", () => {
-  // Begin met expect()
-  // Binnen de eerste haken zet je hetgeen je wilt testen. Dat zal een functie zijn.
-  // Daarna volgt je .toBeSomethingSomething functie,
-  // zoek de geschikte functie in de documentatie.
-  // In dit geval willen we checken of de functie "isNull()" ook daadwerkelijk
-  // Null returned. Dus "toBeNull()" makes the most sense.
   expect(functions.isNull()).toBeNull();
 });
 
-// test("checkValue Should be falsy when argument is undefined", () => {});
+test("checkValue Should be falsy when argument is undefined", () => {
+  expect(functions.checkValue()).toBeUndefined();
+});
 
-// test("User should be Brad Traversy object", () => {});
+test("User should be Brad Traversy object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
 
-// // Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
-// // nu tests maken die werken zonder input van buitenaf:
+// Tests zijn eigenlijk gewoon functies. Om dat te laten zien laten we je
+// nu tests maken die werken zonder input van buitenaf:
 
-// // Less than or greater than
-// test("Should be under or equal to 1600", () => {
-//   const load1 = 800;
-//   const load2 = 800;
-//   // expect(load1 + load2).toBe.....
-// });
+// Less than or greater than
+test("Should be under or equal to 1600", () => {
+  const load1 = 800;
+  const load2 = 600;
+  expect(load1 + load2).toBeLessThan(1600);
+});
 
-// // Regex
-// test("There is no I in team", () => {
-//   //  expect('team').not.to..........;
-// });
+// Regex
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+  // let op: hoofdlettergevoelig ! Om ongevoelig te maken: /I/i
+});
 
-// // Arrays
-// test("Admin should be in usernames", () => {
-//   usernames = ["john", "karen", "admin"];
-//   // expect(usernames).to......
-// });
+// Arrays
+test("Admin should be in usernames", () => {
+  usernames = ["john", "sarah", "admin"];
+  expect(usernames).toContain("admin");
+});

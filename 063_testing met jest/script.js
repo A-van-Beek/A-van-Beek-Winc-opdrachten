09790b84f0597b532b4c8e7eb861738c378e3aa6
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-08-31 14:59:56 +0200

    testing met jest

--- 063_testing met jest/script.js
+++ 063_testing met jest/script.js
@@ -1,2 +1,13 @@
-// hier het script
-console.log("Hallo");
+const functions = {
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  isUndefined: () => undefined,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: "Brad" };
+    user["lastName"] = "Traversy";
+    return user;
+  },
+};
+
+module.exports = functions;

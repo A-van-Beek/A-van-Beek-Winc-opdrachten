737f4a2af4d903b7b7ab73a62e71d345e14c600d
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-02 15:33:14 +0200

    first version test-1

--- 065_Test Driven Development/script.js
+++ 065_Test Driven Development/script.js
@@ -1,5 +1,15 @@
-const addOne = function (numbers) {
-  // Schrijf hier de functie...
+const functions = {
+  add: (num1, num2) => num1 + num2,
+  addOne: () => {
+    const myArray = [31, 57, 12, 5];
+    const output = [];
+    myArray.forEach((i) => {
+      newNumber = i + 1;
+      output.push(newNumber);
+    });
+    return output;
+  },
 };
+// functions.addOne();
 
-module.exports = addOne;
+module.exports = functions;
--- 065_Test Driven Development/script.test.js
+++ 065_Test Driven Development/script.test.js
@@ -1,19 +1,27 @@
-// een werkend test-script om de jest te testen
+// een werkend test-script om de jest te testen.
+// let op: nog geen koppeling met het script
 // arrays
 test("Admin should be in usernames", () => {
   usernames = ["john", "sarah", "admin"];
   expect(usernames).toContain("admin");
 });
 
-// const addOne = require("./add-one.js");
+//koppeling met script-file
+const functions = require("./script");
 
-// test("Add 1 to each item in myArray", function () {
-//   const myArray = [31, 57, 12, 5];
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
 
-//   const unchanged = [31, 57, 12, 5];
-//   const expected = [32, 58, 13, 6];
-//   const output = addOne(myArray);
+// vanaf hier de opdracht:
 
-//   expect(output).toEqual(expected);
-//   expect(myArray).toEqual(unchanged);
-// });
+test("Add 1 to each item in myArray", function () {
+  const myArray = [31, 57, 12, 5];
+
+  const unchanged = [31, 57, 12, 5];
+  const expected = [32, 58, 13, 6];
+  const output = functions.addOne();
+
+  expect(output).toEqual(expected);
+  expect(myArray).toEqual(unchanged);
+});

bbe1115025c8f54472f5f2618b1bee4d68cc95cf
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-02 10:48:25 +0200

    prepare for jest

--- 065_Test Driven Development/script.test.js
+++ 065_Test Driven Development/script.test.js
@@ -1,12 +1,19 @@
-const addOne = require("./add-one.js");
-
-test("Add 1 to each item in myArray", function () {
-  const myArray = [31, 57, 12, 5];
-
-  const unchanged = [31, 57, 12, 5];
-  const expected = [32, 58, 13, 6];
-  const output = addOne(myArray);
-
-  expect(output).toEqual(expected);
-  expect(myArray).toEqual(unchanged);
+// een werkend test-script om de jest te testen
+// arrays
+test("Admin should be in usernames", () => {
+  usernames = ["john", "sarah", "admin"];
+  expect(usernames).toContain("admin");
 });
+
+// const addOne = require("./add-one.js");
+
+// test("Add 1 to each item in myArray", function () {
+//   const myArray = [31, 57, 12, 5];
+
+//   const unchanged = [31, 57, 12, 5];
+//   const expected = [32, 58, 13, 6];
+//   const output = addOne(myArray);
+
+//   expect(output).toEqual(expected);
+//   expect(myArray).toEqual(unchanged);
+// });

18a7c2ff1f27a8829df7b5ebd73abefd64bc1fba
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-03 10:46:45 +0200

    deel 3

--- 065_Test Driven Development/script.js
+++ 065_Test Driven Development/script.js
@@ -21,10 +21,19 @@ const functions = {
     console.log(`uitslag getWordLengths: ${output}`);
     return output;
   },
+  findNeedle: (words, zoek) => {
+    zoekWoord = words.indexOf(zoek);
+    console.log(`Positie van het woord ${zoek} is: ${zoekWoord}`);
+    return zoekWoord;
+  },
 };
 
 functions.add(2, 3);
 functions.addOne([33, 58, 13, 6]);
 functions.getWordLengths(["sun", "potato"]);
+functions.findNeedle(
+  ["house", "office", "phone", "coffin", "needle", "book"],
+  "office"
+);
 
 module.exports = functions;

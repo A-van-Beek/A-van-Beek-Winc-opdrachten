48ac669d80cbe8b64bdb7dc3256f1f8793ddbc40
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-24 10:18:06 +0200

    lesson 7

--- 068_react_try_and_test/src/index.js
+++ 068_react_try_and_test/src/index.js
@@ -20,6 +20,19 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// eenvoudiger door het gebruik van functional components
+function MyApp() {
+  return (
+    <ul>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+    </ul>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("test"));
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

61ca45d40779e0fd72e4ec69373ea214aed369b8
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-27 13:56:12 +0200

    init

---
+++ 070_react_oefening/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <h1>Oefening parent - child</h1>,
+  document.getElementById("root")
+);

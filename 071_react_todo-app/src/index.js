de93795242116b6179c384f2e5266e418623bc2b
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-28 10:19:49 +0200

    init add MyFooter

---
+++ 071_react_todo-app/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from "./components/App";
+import MyFooter from "./components/MyFooter";
+
+ReactDOM.render(
+  <div>
+    <MyFooter />
+  </div>,
+  document.getElementById("root")
+);

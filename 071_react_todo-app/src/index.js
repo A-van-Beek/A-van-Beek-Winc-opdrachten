4a66655ccab93bc2ee27aa188e140280b64a670d
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-28 10:39:03 +0200

    add MyMainContent edit css

--- 071_react_todo-app/src/index.js
+++ 071_react_todo-app/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./components/App";
 import MyFooter from "./components/MyFooter";
-import MyHeader from "./components/MyFooter";
+import MyHeader from "./components/MyHeader";
+import MyMainContent from "./components/MyMainContent";
 
 ReactDOM.render(
   <div>
     <MyHeader />
+    <MyMainContent />
     <MyFooter />
   </div>,
   document.getElementById("root")

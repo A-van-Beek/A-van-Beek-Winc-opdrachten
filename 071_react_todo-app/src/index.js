479be8d2ce3dac0e5846117b081ec662ce63f74d
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-09-28 10:28:59 +0200

    add basic header

--- 071_react_todo-app/src/index.js
+++ 071_react_todo-app/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./components/App";
 import MyFooter from "./components/MyFooter";
+import MyHeader from "./components/MyFooter";
 
 ReactDOM.render(
   <div>
+    <MyHeader />
     <MyFooter />
   </div>,
   document.getElementById("root")

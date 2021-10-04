25d1ad7b926ee2f8f91a71042d2a17ce98629e77
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-04 11:29:07 +0200

    tussenstand

--- 076_react_jokes/src/App.js
+++ 076_react_jokes/src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
 import Joke from "./Joke";
 
-function App() {
-  return (
-    <div className="jokes">
-      <Joke
-        joke={{
-          question: "What is a good joke?",
-          punchline: "The one that makes you laugh",
-          imgUrl: "http://placekitten.com/300/200",
-        }}
-      />
-    </div>
-  );
-}
+return (
+  <div className="jokes">
+    <Joke
+      joke={{
+        question: "What is a good joke?",
+        punchline: "The one that makes you laugh",
+        imgUrl: "http://placekitten.com/300/200",
+      }}
+    />
+  </div>
+);
 
 export default App;

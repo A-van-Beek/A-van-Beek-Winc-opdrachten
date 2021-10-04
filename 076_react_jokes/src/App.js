f09beec872e1a972bb6b26685b300d789f86b849
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-04 10:35:02 +0200

    initial draft

--- 076_react_jokes/src/App.js
+++ 076_react_jokes/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Joke from "./Joke";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="jokes">
+      <Joke
+        joke={{
+          question: "What is a good joke?",
+          punchline: "The one that makes you laugh",
+          imgUrl: "http://placekitten.com/300/200",
+        }}
+      />
     </div>
   );
 }
---
+++ 076_react_jokes/src/Joke.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Joke(props) {
+  console.log(props);
+  return (
+    <div className="joke">
+      <img src={props.joke.imgUrl} />
+      <h3>{props.joke.question}</h3>
+      <h4>{props.joke.punchline}</h4>
+    </div>
+  );
+}
+
+export default Joke;

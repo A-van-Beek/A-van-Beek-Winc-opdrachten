5a8242934153ac85c48823f5733c4da8f06360de
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-05 09:16:25 +0200

    fixing alt text img

--- 076_react_jokes/src/Joke.js
+++ 076_react_jokes/src/Joke.js
@@ -4,7 +4,7 @@ function Joke(props) {
   console.log(props);
   return (
     <div className="joke">
-      <img src={props.joke.imgUrl} />
+      <img src={props.joke.imgUrl} alt="alt-tekst" />
       <h3>{props.joke.question}</h3>
       <h4>{props.joke.punchline}</h4>
     </div>

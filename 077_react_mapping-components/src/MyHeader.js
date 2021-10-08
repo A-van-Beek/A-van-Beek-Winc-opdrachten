af5c520a80967fee484ee64cee6864132562b5dc
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-08 09:20:23 +0200

    add title

--- 077_react_mapping-components/src/MyHeader.js
+++ 077_react_mapping-components/src/MyHeader.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-const styleBasic = {
-  color: "black",
-  backgroundColor: "#FF2D00",
-  fontSize: "20px",
-};
-
 function MyHeader() {
-  return (
-    <header className="navbar" style={styleBasic}>
-      Using .map function in React
-    </header>
-  );
+  return <header className="title">Using .map function in React</header>;
 }
 
 export default MyHeader;

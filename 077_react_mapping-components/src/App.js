78485fed0c38df790bcee88ea08054a306306928
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-08 12:20:59 +0200

    solution video

--- 077_react_mapping-components/src/App.js
+++ 077_react_mapping-components/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import MyHeader from "./MyHeader";
+import productsData from "./vschoolProducts";
+import Product from "./Product";
 
 function App() {
+  const productComponent = productsData.map((product) => {
+    return <Product key={product.id} product={product} />;
+  });
+
   return (
     <div>
       <MyHeader />
+      <div className="products">{productComponent}</div>;
     </div>
   );
 }

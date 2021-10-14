502e558a8f3c2d957472d85ad06ac3df795f291a
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-14 11:16:23 +0200

    final 84

--- 084_react_handling-events/src/App.js
+++ 084_react_handling-events/src/App.js
@@ -1,32 +1,28 @@
 import React from "react";
 
-// Given a stateless functional component, add state to it
-// state should have a property called `isLoggedIn` which is a boolean
-// (true if logged in, false if not)
-// Then, give your best shot at rendering the word "in" if the user is logged in
-// or "out" if the user is logged out.
+function handleClick() {
+  console.log("I was clicked");
+}
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: true,
-    };
-  }
+function handleMouseOver() {
+  console.log("I flew over the man");
+}
 
-  render() {
-    let wordDisplay;
-    if (this.state.isLoggedIn === true) {
-      wordDisplay = "in";
-    } else {
-      wordDisplay = "out";
-    }
-    return (
-      <div>
-        <h1>You are currently logged {wordDisplay}</h1>
-      </div>
-    );
-  }
+// https://reactjs.org/docs/events.html#supported-events
+
+function App() {
+  return (
+    <div>
+      <img
+        src="https://www.fillmurray.com/200/100"
+        alt="tekst"
+        onMouseOver={handleMouseOver}
+      />
+      <br />
+      <br />
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
 }
 
 export default App;

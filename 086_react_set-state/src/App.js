9f5f72058a7b3fba0f540ee59c2d7018000e7719
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-15 11:24:16 +0200

    add button 2

--- 086_react_set-state/src/App.js
+++ 086_react_set-state/src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
       count: 0,
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
   }
 
   handleClick() {
@@ -17,12 +18,20 @@ class App extends React.Component {
     });
   }
 
+  handleClick2() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count * 2,
+      };
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.count}</h1>
         <button onClick={this.handleClick}>Change!</button>
-        <ChildComponent count={this.state.count} />
+        <button onClick={this.handleClick2}>Change dubble!</button>
       </div>
     );
   }

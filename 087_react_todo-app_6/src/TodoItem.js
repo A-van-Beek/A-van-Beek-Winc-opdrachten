991a9514ab127658cc92b7abf15b381be2f9fbb4
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-15 12:36:39 +0200

    input code from video

--- 087_react_todo-app_6/src/TodoItem.js
+++ 087_react_todo-app_6/src/TodoItem.js
@@ -1,13 +1,4 @@
-/**
- * Challenge: Get rid of our warning about not having an onChange on our input.
- * For now, the function that runs onChange can simply console.log something.
- */
-
 import React from "react";
-
-function changeHandler() {
-  console.log("You clicked !");
-}
 
 function TodoItem(props) {
   return (
@@ -15,7 +6,7 @@ function TodoItem(props) {
       <input
         type="checkbox"
         checked={props.item.completed}
-        onClick={changeHandler}
+        onChange={() => console.log("Changed!")}
       />
       <p>{props.item.text}</p>
     </div>

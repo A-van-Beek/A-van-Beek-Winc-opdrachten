97a17aef120601ccbc1857da9cdd9f2dcadee053
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-26 12:04:57 +0200

    init 091

---
+++ 091_react_fetch-data-api/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.co/
+// https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch("https://swapi.co/api/people/1");
+  }
+
+  render() {
+    return <div>Code goes here</div>;
+  }
+}
+
+export default App;

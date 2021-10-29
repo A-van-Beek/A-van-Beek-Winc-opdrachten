f55a18610cab197c6dc9a40a4d75863ad5731583
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-10-29 09:50:20 +0200

    tussenstand

--- 096_react_mile-stone-project/src/App.js
+++ 096_react_mile-stone-project/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import "./style.css";
+import Header from "./Header";
 
 function App() {
-  return <div>teksten hier </div>;
+  return (
+    <div>
+      <Header />
+      <h3>Hello world !</h3>
+    </div>
+  );
 }
 
 export default App;

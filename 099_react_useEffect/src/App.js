eb448f73af6c6b208e7fe36fecb2b57deaa0b66e
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-11-01 11:01:01 +0100

    final

--- 099_react_useEffect/src/App.js
+++ 099_react_useEffect/src/App.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+// Side effects?
+// Network request
+// Manual DOM manipulation
+// Event listeners or timeouts and intervals
+
+import React, { useState, useEffect } from "react";
+import randomcolor from "randomcolor";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [answer, setAnswer] = useState("Yes");
+  const [color, setColor] = useState("");
+  const [teller, setTeller] = useState(0);
 
   function increment() {
     setCount((prevCount) => prevCount + 1);
@@ -12,23 +19,42 @@ function App() {
     setCount((prevCount) => prevCount - 1);
   }
 
-  function toggleAnswer() {
-    setAnswer((prevAnswer) => prevAnswer + "Yes");
-  }
+  useEffect(() => {
+    setColor(randomcolor());
+  }, [count]);
+  /**
+   * useEffect heeft twee parameters,
+   * de eerste is een call-back function
+   * ______()setcolor to randomcolor
+   * daarna kan je een array invoeren van de variabelen
+   * waarnaar gekeken moet worden om het effect te triggeren
+   * Doe je dat niet dan krijg een een oneindige loop
+   * laat je het array leeg, dan wordt hij alleen getriggerd
+   * bij de eerste run (en niet bij wijzigen)
+   */
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTeller((prevTeller) => prevTeller + 1);
+      /**
+       * dit is een teller die oneindig doorgaat,
+       * via de return kan je een functie schrijven die
+       * deze weer opruimt
+       */
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={increment}>Increment!</button>
-      <button onClick={decrement}>Decrement!</button>
+      <h1 style={{ color: color }}>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
       <br />
       <br />
-      <h1>Is it working?</h1>
-      <button onClick={toggleAnswer}>toggle answer</button>
-      <br />
-      <br />
-      <br />
-      <div>{answer}</div>
+      <h1>{teller}</h1>
     </div>
   );
 }

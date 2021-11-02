3052dd7ae1f0f2c8841454347b430f0a05ac9f10
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-11-02 11:05:55 +0100

    rename listGrocerys naar groceryItems

--- 100_react_my_groceries_app/src/Components/App.js
+++ 100_react_my_groceries_app/src/Components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "./Header";
 
+/**
+ * leuke plaatjes https://tessakortenbach.nl/2020/10/17/boodschappen-doen-liedje-voor-peuters-en-kleuters/
+ */
+
 function App() {
   return (
     <div>

f5a7b0ab72e51805ba3f0565b5a7508b898c90cd
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-02 12:00:47 +0100

    changes t/m lesson 17

--- 101_react_second course/index.js
+++ 101_react_second course/index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+function MyNavbar() {
+  return (
+    <nav className="nav">
+      <h1>website name</h1>
+      <ul className="nav-items">
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+function MyContent() {
+  return <h1>hier mijn eigen tekst</h1>;
+}
+
+ReactDOM.render(
+  <div>
+    <MyNavbar />
+    <MyContent />
+  </div>,
+  document.getElementById("root")
+);

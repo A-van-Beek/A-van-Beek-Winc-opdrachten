1bd0e49f02e5c2519f7c69de4dd6d074ce0a373f
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-03 11:45:58 +0100

    aanpassingen nav-deel

--- 103_react_second_course_funfacts/src/components/Main.js
+++ 103_react_second_course_funfacts/src/components/Main.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Main() {
-  return <h1>main content goes here</h1>;
+  return (
+    <div className="main-container">
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul>
+        <li className="main-item">Was first released in 2013</li>
+        <li className="main-item">Was originally created by Jordan Walke</li>
+        <li className="main-item">Has well over 100K stars on GitHub</li>
+        <li className="main-item">Is maintained by Facebook</li>
+        <li className="main-item">
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Main;
--- 103_react_second_course_funfacts/src/components/Navbar.js
+++ 103_react_second_course_funfacts/src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import reactIconSmall from "../images/reactIconSmall.png";
 
 function Navbar() {
-  return <h1>Navbar content goes here</h1>;
+  return (
+    <nav className="nav-container">
+      <img src={reactIconSmall} alt="broken icon" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
 }
 
 export default Navbar;

ee3491ab3e8b3d92482a4d1e9ec035b193894aa5
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-07 11:17:53 +0100

    interests

--- 104_react_second_course_solo_1/src/App.js
+++ 104_react_second_course_solo_1/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Info from "./components/Info.js";
 import About from "./components/About.js";
+import Interests from "./components/Interests";
 
 function App() {
   return (
     <div>
       <Info />
       <About />
+      <Interests />
     </div>
   );
 }
--- 104_react_second_course_solo_1/src/components/About.js
+++ 104_react_second_course_solo_1/src/components/About.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <div className="about--title">About</div>
-      <div className="about--text">
+    <div className="about">
+      <div className="sub--title">About</div>
+      <div className="sub--text">
         I am a frontend developer with a particular interest in making things
         simple and automating daily tasks. I try to keep up with security and
         best practices, and am always looking for new things to learn.

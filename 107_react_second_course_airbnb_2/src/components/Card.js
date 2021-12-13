aabd617eb4129bbec6f8541d61687e71a0d3bc76
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-13 13:55:14 +0100

    add badgeText

--- 107_react_second_course_airbnb_2/src/components/Card.js
+++ 107_react_second_course_airbnb_2/src/components/Card.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={`../images/${props.img}`} alt="oeps" className="card--image" />
       <div className="card--stats">
         <img src="../images/star.png" alt="oeps" className="card--star" />

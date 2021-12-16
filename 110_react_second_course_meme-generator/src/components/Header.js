97644209b18cc12b94c70bb90d1bb00a7350c6e5
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-16 12:30:13 +0100

    import memeData random memeImage

--- 110_react_second_course_meme-generator/src/components/Header.js
+++ 110_react_second_course_meme-generator/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
-      <img className="header--icon" src="./images/Troll Face.png" alt="oeps" />
+      <img className="header--img" src="./images/Troll Face.png" alt="oeps" />
       <div className="header--title">Meme Generator</div>
       <div className="header--project">React Course - Project 3</div>
     </header>

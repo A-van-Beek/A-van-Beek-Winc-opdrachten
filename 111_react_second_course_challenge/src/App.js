22e118bb220ca43ba686845f6fe37b218d287aa4
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-17 09:10:16 +0100

    next step

--- 111_react_second_course_challenge/src/App.js
+++ 111_react_second_course_challenge/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import React from "react";
 
 function App() {
+  const thingsArray = ["Thing 1", "Thing 2"];
+  const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+
+  function addItem() {
+    const newThingText = `Thing ${thingsArray.length + 1}`;
+    thingsArray.push(newThingText);
+    console.log(thingsArray);
+  }
+
   return (
     <div>
-      <h1>tekst</h1>
+      <button onClick={addItem}>Add Item</button>
+      {thingsElements}
     </div>
   );
 }

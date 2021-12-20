ed055ef4e9e2f200aa29f5c97e240c6475f512cc
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-20 14:08:29 +0100

    intro prevCount

--- 112_react_2_course_counter/src/components/Counter.js
+++ 112_react_2_course_counter/src/components/Counter.js
@@ -7,8 +7,26 @@ function Counter() {
     setCount(count + 1);
   }
 
+  /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+
+     * Note 2: even shorter:
+     *     function add() {
+     *         setCount(prevCount => prevCount + 1)
+     *     }
+     */
+
   function minus() {
-    setCount(count - 1);
+    setCount(function (prevCount) {
+      return prevCount - 1;
+    });
   }
   return (
     <div className="counter">

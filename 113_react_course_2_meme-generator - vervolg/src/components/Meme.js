834ec1f2c1ba6e2aaeafd9a9ace12420893cf217
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-21 10:20:38 +0100

    h

--- 113_react_course_2_meme-generator - vervolg/src/components/Meme.js
+++ 113_react_course_2_meme-generator - vervolg/src/components/Meme.js
@@ -2,21 +2,26 @@ import React from "react";
 import memesData from "../memesData";
 
 function Meme() {
+  const [memeImage, setMemeImage] = React.useState(
+    "https://i.imgflip.com/1jwhww.jpg"
+  );
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-    console.log(url);
+    setMemeImage(memesArray[randomNumber].url);
   }
+
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button onClick={getMemeImage} className="form--button">
+        <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
+      <img src={memeImage} alt="oeps" />
     </main>
   );
 }

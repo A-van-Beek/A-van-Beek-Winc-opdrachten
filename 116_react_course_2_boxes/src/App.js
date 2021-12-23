944eab0eb086e2afe1e00470f6d30a2034575e05
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2021-12-23 11:51:27 +0100

    init 116

---
+++ 116_react_course_2_boxes/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  // const [user, setUser] = React.useState("Jane");
+
+  return (
+    <main>
+      <Header />
+      {/* <Body /> */}
+    </main>
+  );
+}
+
+export default App;

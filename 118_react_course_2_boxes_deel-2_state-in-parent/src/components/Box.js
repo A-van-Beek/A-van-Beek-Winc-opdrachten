ab829d70c5ae3bf41cd120066817a9823ae41523
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-01-11 10:53:50 +0100

    2x toggle function

--- 118_react_course_2_boxes_deel-2_state-in-parent/src/components/Box.js
+++ 118_react_course_2_boxes_deel-2_state-in-parent/src/components/Box.js
@@ -1,19 +1,9 @@
 import React from "react";
 
 export default function Box(props) {
-  const [on, setOn] = React.useState(props.on);
-  /**
-op deze manier maak je een useState voor elke individuele box
-kan in een aantal specifieke gevallen de juiste werkwijze zijn,
-maar meestal is de volgende methode beter.
-   */
   const styles = {
-    backgroundColor: on ? "#222222" : "transparent",
+    backgroundColor: props.on ? "#222222" : "transparent",
   };
 
-  function toggle() {
-    setOn((prevOn) => !prevOn);
-  }
-
-  return <div style={styles} className="box" onClick={toggle}></div>;
+  return <div style={styles} className="box" onClick={props.toggle}></div>;
 }

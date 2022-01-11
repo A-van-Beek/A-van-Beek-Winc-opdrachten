29d58ba62da108f94b7c2c9cbaff3499a4bd84dc
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-01-11 11:50:15 +0100

    final

--- 119_react_second_course_jokes_conditional-rendering/src/Joke.js
+++ 119_react_second_course_jokes_conditional-rendering/src/Joke.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 export default function Joke(props) {
-  /**
-   * Challenge:
-   * - Create state `isShown` (boolean, default to `false`)
-   * - Add a button that toggles the value back and forth
-   */
+  const [isShown, setIsShown] = React.useState(false);
+
+  function toggleShown() {
+    setIsShown((prevShown) => !prevShown);
+  }
+
   return (
     <div>
       {props.setup && <h3>{props.setup}</h3>}
-      <p>{props.punchline}</p>
+      {isShown === true && <p>{props.punchline}</p>}
+      <button onClick={toggleShown}>show punchline</button>
       <hr />
     </div>
   );

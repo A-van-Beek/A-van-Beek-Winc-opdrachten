80416b0b1c7ec1847b96b71a8c73450a2d91d4b1
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-01-11 12:09:23 +0100

    my solution number unread display or not

--- 120_react_course_2_messages_conditional-rendering/src/App.js
+++ 120_react_course_2_messages_conditional-rendering/src/App.js
@@ -3,14 +3,16 @@ import React from "react";
 export default function App() {
   const [messages, setMessages] = React.useState(["a", "b"]);
 
-  const numberMessages = messages.length;
   /**
    * Challenge:
    * - Only display the <h1> below if there are unread messages
    */
   return (
     <div>
-      <h1>You have {numberMessages} unread messages!</h1>
+      {messages.length > 0 && (
+        <h1>You have {messages.length} unread messages!</h1>
+      )}
+      {messages.length === 0 && <h1>You have no messages!</h1>}
     </div>
   );
 }

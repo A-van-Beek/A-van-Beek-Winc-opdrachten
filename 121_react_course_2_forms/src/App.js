72eccbbd779f2d6b49bbcc92d1b19660c87e7e4c
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-01-17 13:53:57 +0100

    first and last name

--- 121_react_course_2_forms/src/App.js
+++ 121_react_course_2_forms/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 
 /* 
@@ -5,9 +6,36 @@ dit is dezelfde file als Form.js in de video
 */
 
 export default function App() {
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  console.log(formData);
+
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
   return (
     <form>
-      <input type="text" placeholder="First Name" />
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+      />
     </form>
   );
 }

baa53894b8d25b86b3f12224b954b3668bd36896
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-01-17 11:54:04 +0100

    final

--- 120_react_course_2_messages_conditional-rendering/src/App.js
+++ 120_react_course_2_messages_conditional-rendering/src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 
 export default function App() {
   const [messages, setMessages] = React.useState(["a", "b"]);
+  // const numMessages =
 
-  /**
-   * Challenge:
-   * - Only display the <h1> below if there are unread messages
-   */
   return (
     <div>
-      {messages.length > 0 && (
-        <h1>You have {messages.length} unread messages!</h1>
+      {messages.length === 0 ? (
+        <h1>You're all caught up!</h1>
+      ) : (
+        <h1>
+          You have {messages.length} unread{" "}
+          {messages.length > 1 ? "messages" : "message"}
+        </h1>
       )}
-      {messages.length === 0 && <h1>You have no messages!</h1>}
     </div>
   );
 }

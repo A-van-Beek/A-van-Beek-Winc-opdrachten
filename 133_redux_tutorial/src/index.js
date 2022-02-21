224c9a8124c54f344af4be3c85426e812af89e78
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-02-21 14:09:31 +0100

    optie 1, alles in 1 file

--- 133_redux_tutorial/src/index.js
+++ 133_redux_tutorial/src/index.js
@@ -2,6 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+
+// STORE -> globalized state
+
+// ACTION increment
+const increment = () => {
+  return {
+    type: "INCREMENT", // je ziet ook vaak name:
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// REDUCER, deze geeft markering omdat hij een default case verwacht.
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>

38899304c16d5423958712ee32008497808aa959
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-01 14:50:45 +0100

    delete bugReducer, init data store

--- 136_react-redux_songsaver_v2/src/reducers/allReducers.js
+++ 136_react-redux_songsaver_v2/src/reducers/allReducers.js
@@ -1,10 +1,8 @@
 // import store from "../store";
 import { combineReducers } from "redux";
-import bugReducer from "./bugReducer";
 import songReducer from "./songReducer";
 
 const allReducers = combineReducers({
-  bugReducer: bugReducer,
   songReducer: songReducer,
 });
 
--- 136_react-redux_songsaver_v2/src/reducers/songReducer.js
+++ 136_react-redux_songsaver_v2/src/reducers/songReducer.js
@@ -1,6 +1,23 @@
 import * as actions from "../actions/actionTypes";
 
-function songReducer(state = [], action) {
+const initialState = [
+  {
+    id: 0,
+    title: "Bohemian Rhapsody",
+    artist: "Queen",
+    genre: "rock",
+    rating: 5,
+  },
+  {
+    id: 1,
+    title: "Dancing Queen",
+    artist: "Abba",
+    genre: "pop",
+    rating: 1,
+  },
+];
+
+function songReducer(state = initialState, action) {
   switch (action.type) {
     case actions.SONG_ADDED:
       return [...state, { descriptiveArray: action.payload.descriptiveArray }];

9caa0f11bf32559b628f67d8ee1d1dc314288809
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-04 09:56:04 +0100

    tweakje

--- 136_react-redux_songsaver_v2/src/components/SongList.js
+++ 136_react-redux_songsaver_v2/src/components/SongList.js
@@ -6,7 +6,7 @@ function SongList() {
 
   const songItems = listSongs.map((item) => (
     <li key={item.id}>
-      {item.title} ({item.artist}) met score {item.rating}. Genre is:
+      {item.title} ({item.artist}) met score {item.rating}. Genre is:{" "}
       {item.genre}
     </li>
   ));

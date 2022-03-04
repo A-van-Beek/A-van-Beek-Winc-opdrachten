2bc3ed48adbe524d956edcc1702dec68d4d3d5dd
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-04 10:17:31 +0100

    connect the URL

--- 138_react-routing/src/index.js
+++ 138_react-routing/src/index.js
@@ -1,7 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./main.css";
 
-let rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);

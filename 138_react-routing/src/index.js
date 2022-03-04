c056f92b5b8a8c6f94b58ac6a9e85938a0f916c9
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-04 10:56:45 +0100

    reading URL Params

--- 138_react-routing/src/index.js
+++ 138_react-routing/src/index.js
@@ -1,8 +1,9 @@
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
+import Expenses from "./routes/Expenses";
+import Invoices from "./routes/Invoices";
+import Invoice from "./routes/Invoice";
 
 const rootElement = document.getElementById("root");
 render(
@@ -10,7 +11,9 @@ render(
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route
           path="*"
           element={

2762010353b55d1b19851e7063393b984d7af064
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-04 15:29:30 +0100

    clean-up

--- 137_react-redux_songsaver_v3/src/actions/actionTypes.js
+++
@@ -1,1 +0,0 @@
-export const SONG_ADDED = "songAdded";
--- 137_react-redux_songsaver_v3/src/actions/actions.js
+++
@@ -1,8 +0,0 @@
-import * as actions from "./actionTypes";
-
-export const songAdded = (description) => ({
-  type: actions.SONG_ADDED,
-  payload: {
-    description: description,
-  },
-});

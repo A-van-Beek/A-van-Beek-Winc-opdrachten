9dd306958081899d093ed35bbbf1b906040a127c
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-14 11:24:53 +0100

    sort clients last_name, first_name

--- 140_dentist_react-master/src/routes/Clients.js
+++ 140_dentist_react-master/src/routes/Clients.js
@@ -1,22 +1,37 @@
 import React from "react";
 import store from "../store";
+import "./Clients.css";
 
 const clientData = store.getState().clientReducer;
 
+/**
+ * sortering cliënten, alfabetich achternaam - voornaam
+ */
 clientData.sort((a, b) => {
-  let fa = a.first_name;
-  let fb = b.first_name;
-  if (fa < fb) {
+  if (a.last_name < b.last_name) {
     return -1;
   }
-  if (fa > fb) {
+  if (a.last_name > b.last_name) {
+    return 1;
+  }
+  if (a.first_name < b.first_name) {
+    return -1;
+  }
+  if (a.first_name > b.first_name) {
     return 1;
   }
   return 0;
 });
 
 const clientItems = clientData.map((item) => (
-  <li key={item.id}>{item.first_name}</li>
+  <li key={item.id}>
+    {item.first_name} {item.last_name}
+    {"........."}
+    <button className="delete-btn">
+      <i className="gg-trash trash-icon"></i>
+    </button>
+    {"........."}
+  </li>
 ));
 
 export default function Clients() {

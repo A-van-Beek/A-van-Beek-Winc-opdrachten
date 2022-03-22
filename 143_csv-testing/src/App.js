abfe8924f74b45c70ec89270d80233b4600e5852
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-22 15:34:32 +0100

    testing and testing

--- 143_csv-testing/src/App.js
+++ 143_csv-testing/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import studenten3 from "./studenten3.csv";
+import studenten from "./data/studenten.csv";
 import * as d3 from "d3";
 
-d3.csv(studenten3).then(function (data) {
-  console.log(data);
+// deze code werkt
+d3.csv(studenten3).then(function (studentInfo) {
+  console.log(studentInfo);
 });
 
+// const data = d3.dsv(";", studenten, (d) => {
+//   return {
+//     student_id: d.student_id,
+//     first_name: d.first_name,
+//     last_name: d.last_name,
+//     age: +d.age,
+//   };
+// });
+// console.log(data);
+
 function App() {
-  return <div>hier de tekst</div>;
+  return <div>hier de tekst: </div>;
 }
 
 export default App;

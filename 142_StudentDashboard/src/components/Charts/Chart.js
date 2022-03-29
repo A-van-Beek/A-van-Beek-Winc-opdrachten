d2992c8f73b081ae75ae5d86839bc9db64c02ff8
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-03-29 09:31:53 +0200

    testing charts

--- 142_StudentDashboard/src/components/Charts/Chart.js
+++ 142_StudentDashboard/src/components/Charts/Chart.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import store from "../../store";
+import * as v from "victory";
 
 class Chart extends React.Component {
   constructor() {
@@ -13,12 +14,44 @@ class Chart extends React.Component {
   handleClick(student_id) {
     console.log(student_id);
   }
+
   render() {
+    const data = [
+      { quarter: 1, earnings: 13000 },
+      { quarter: 2, earnings: 16500 },
+      { quarter: 3, earnings: 14250 },
+      { quarter: 4, earnings: 12000 },
+    ];
+
     return (
       <>
         <Header page="Grafisch overzicht" />
         <Navbar />
         <h1>hier de grafieken</h1>
+        <v.VictoryChart
+          // domainPadding will add space to each side of VictoryBar to
+          // prevent it from overlapping the axis
+          domainPadding={20}
+        >
+          <v.VictoryAxis
+            // tickValues specifies both the number of ticks and where
+            // they are placed on the axis
+            tickValues={[1, 2, 3, 4]}
+            tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+          />
+          <v.VictoryAxis
+            dependentAxis
+            // tickFormat specifies how ticks should be displayed
+            tickFormat={(x) => `$${x / 1000}k`}
+          />
+          <v.VictoryBar
+            data={data}
+            // data accessor for x values
+            x="quarter"
+            // data accessor for y values
+            y="earnings"
+          />
+        </v.VictoryChart>
       </>
     );
   }

c663e16efb197aa947d3ff517e26e76194039a18
A-van-Beek <81556170+A-van-Beek@users.noreply.github.com>
2022-05-16 09:20:03 +0200

    grote verhuizing

--- 050_Data_ophalen_met_JavaScript/script.js
+++
@@ -1,7 +0,0 @@
-fetch("http://swapi.dev/api/planets/1/")
-  .then((response) => response.json())
-  .then((myJson) => {
-    // document.body.innerHTML =
-    document.getElementById("data").innerHTML =
-      "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>";
-  });
